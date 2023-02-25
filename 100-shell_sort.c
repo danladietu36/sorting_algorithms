4898a6a5b47b6b5ba19a1c69b2ffcefb74a84f39
Danladi Etu <danldietugithub@gmail.com>
2023-02-25 13:33:27 -0800

    shell_sort

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,8 +1,15 @@
 #include "sort.h"
 
-void swaps_int(int *a, int *b)
+/**
+ * swap_ints - Swap two integers in an array.
+ * @a: The first integer to swap.
+ * @b: The second integer to swap.
+ */
+void swap_ints(int *a, int *b)
 {
-	int tmp = *a;
+	int tmp;
+
+	tmp = *a;
 	*a = *b;
 	*b = tmp;
 }
@@ -27,12 +34,12 @@ void shell_sort(int *array, size_t size)
 
 	for (; gap >= 1; gap /= 3)
 	{
-		for (i =gap; i < size; i++)
+		for (i = gap; i < size; i++)
 		{
-			j = 1;
+			j = i;
 			while (j >= gap && array[j - gap] > array[j])
 			{
-				swaps_int(array + j, array + (j - gap));
+				swap_ints(array + j, array + (j - gap));
 				j -= gap;
 			}
 		}
